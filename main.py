2ec196a23c0cf4dc05aaf0a61b06d27971c571a2
Birm <birm@rbirm.us>
2016-08-02 00:39:25 -0400

    finished basic functionality

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.button import Button
 from kivy.graphics import Color, Rectangle
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+# question counters
+class QuestionTracker(object):
+    def __init__(self):
+        self.number = 0
+        self.position = 0
 
-class InfoScreen(GridLayout):
+    def newq(self):
+        self.number = self.number + 1
+        self.position = self.number
+        return self.number
+
+qtrack = QuestionTracker()
+
+class InfoLayout(GridLayout):
     """Take basic input for form."""
     def __init__(self, **kwargs):
-        super(InfoScreen, self).__init__(**kwargs)
+        super(InfoLayout, self).__init__(**kwargs)
         # title input
         self.cols = 2
         self.add_widget(Label(text='Title'))
@@ -47,7 +60,75 @@ class InfoScreen(GridLayout):
         instance.rect.size = instance.size
 
     def button1Press(self,btn):
-        print ("title: " + self.title.text + "\n" + "and so on")
+        print ("--- !" +  self.title.text)
+        print ("title: " + self.title.text)
+        print ("owner: " + self.owner.text)
+        print ("behavior: ")
+        print ("    intro-message: " + self.intro.text)
+        print ("    outro-message: " + self.outro.text)
+        print ("questions: ")
+        sm.switch_to(AddQuestionScreen(name='Add Question' +
+                                       str(qtrack.newq())))
+
+
+class AddQuestionLayout(GridLayout):
+    """Add a question."""
+    def __init__(self, **kwargs):
+        super(AddQuestionLayout, self).__init__(**kwargs)
+        # question title input
+        self.cols = 2
+        self.add_widget(Label(text='Title'))
+        self.title = TextInput(multiline=False)
+        self.add_widget(self.title)
+        # quesiton type
+        self.add_widget(Label(text='Type'))
+        self.qtype = TextInput(multiline=False)
+        self.add_widget(self.qtype)
+        # validation rules (TODO fix this to have select types)
+        self.add_widget(Label(text='Validation'))
+        self.validation = TextInput(multiline=True)
+        self.add_widget(self.validation)
+        # add this question
+        self.button1 = Button(text='Save and add another')
+        self.button1.bind(on_press=self.button1Press)
+        self.add_widget(self.button1)
+        self.button2 = Button(text='Finish')
+        self.button2.bind(on_press=self.button2Press)
+        self.add_widget(self.button2)
+        #change canvas options
+        with self.canvas.before:
+            Color(0.4, 0.4, 0.4, 1)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.bind(size=self._update_rect, pos=self._update_rect)
+
+    def _update_rect(self, instance, value):
+        instance.rect.pos = instance.pos
+        instance.rect.size = instance.size
+
+    def button1Press(self,btn):
+        print ("  - title: " + self.title.text )
+        print ("    type: " + self.qtype.text )
+        if self.validation.text:
+            print ("    validation: " + self.validation.text )
+        sm.switch_to(AddQuestionScreen(name='Add Question' +
+                                       str(qtrack.newq())))
+
+    def button2Press(self,btn):
+        print ("\nDONE")
+
+class InfoScreen(Screen):
+    def __init__(self, **kwargs):
+        super(InfoScreen, self).__init__(**kwargs)
+        self.add_widget(InfoLayout(padding=[20,20,20,20]))
+
+class AddQuestionScreen(Screen):
+    def __init__(self, **kwargs):
+        super(AddQuestionScreen, self).__init__(**kwargs)
+        self.add_widget(AddQuestionLayout(padding=[20,20,20,20]))
+
+# manage screens
+sm = ScreenManager()
+sm.add_widget(InfoScreen(name='Intro'))
 
 
 
@@ -55,8 +136,7 @@ class formMaker(App):
     """Generate everything and run the application"""
     def build(self):
         self.icon = 'icon.png'
-        inf = InfoScreen(padding=[20,20,20,20])
-        return inf
+        return sm
 
 
 if __name__ == '__main__':
